b59543d33a98d2f7dc0ee510433b5cc3c56b3937
CubeStar1 <avinash.anish04@gmail.com>
2024-12-22 23:20:13 +0530

    Fix: Removed mdx temporarily

--- app/(app)/visualizer/heap/page.tsx
+++ app/(app)/visualizer/heap/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
 import { HeapVisualizer } from "@/components/visualizer/heap/heap-visualizer"
-import Content from "./heap.mdx"
+// import Content from "./heap.mdx"
+
+function Content() {
+  return <div className="text-center text-2xl font-bold">To be implemented</div>
+}
 
 export default function HeapPage() {
   return <HeapVisualizer content={<Content />} />
--- app/(app)/visualizer/queue-applications/page.tsx
+++ app/(app)/visualizer/queue-applications/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
 import { MessageQueueVisualizer } from "@/components/visualizer/queue-applications/message-queue-visualizer"
-import Content from "./message-queue.mdx"
+// import Content from "./message-queue.mdx"
+
+function Content() {
+  return <div className="text-center text-2xl font-bold">To be implemented</div>
+}
 
 export default function MessageQueuePage() {
   return <MessageQueueVisualizer content={<Content />} />
